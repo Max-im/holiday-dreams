d0e06f600a842bb49e1cf4a58a0dba5246247458
Maxim Pozhidaev <pogidaevmo@gmail.com>
2016-11-02 02:39:38 +0300

    remade masonry

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 (function($) {
-     
-     
+		 
+		 
 	// owlCarousel
 	var i = 1;
 	for( i; i < 4; i++ ){
@@ -16,14 +16,6 @@
 	}
 
 
-	// masonry
-	$('.idias').masonry({
-		// options
-		itemSelector: '.idia',
-		columnWidth: '.idia',
-		gutter: 20
-	});
-
 
 
 
@@ -35,10 +27,10 @@
 	
 	// press enter key
 	$('body').keyup(function(e){
-	    if(e.keyCode==13){
-	        findImg();
-	        return false;
-	    }
+			if(e.keyCode==13){
+					findImg();
+					return false;
+			}
 	});
 
 	function findImg(){
@@ -48,12 +40,57 @@
 			format: "json"
 		}, 
 			function(result){
+					var modWidthMax;
+					var modWidthMin;
+					var contatinerWidth = $('.container').width();
+					
+					if(contatinerWidth === 992){
+						modWidthMax = 620;
+						modWidthMin = 300; 
+					}else if(contatinerWidth === 768){
+						modWidthMax = 490;
+						modWidthMin = 235; 
+					}else{
+						modWidthMax = 300;
+						modWidthMin = 300; 
+					}
+					console.log($('.container').width());
+
 				$.each(result.images, function( i, item){
+					var img = new Image();     
+					img.src = item.imageurl;
+
+						img.onload = function(e){
+							var imgWidth = e.target.width;
+							var imgHeight = e.target.height;
+							var portretCheck = imgWidth / imgHeight;
+
+							if( portretCheck > 1.4){
+								$('.idia-'+[i+1]).width(modWidthMax);
+								$('.idia-'+[i+1]).height(modWidthMax/portretCheck);
+							}
+							else{
+								$('.idia-'+[i+1]).width(modWidthMin);
+								$('.idia-'+[i+1]).height(modWidthMin/portretCheck);
+							};
+
+							// masonry
+							// need to start after setting 
+							// width and height!!!!!
+							$('.idias').masonry({
+							  columnWidth: modWidthMin,
+							  itemSelector: '.idia',
+							  gutter: 20
+							})
+
+						}
+
 					var text = item.word;
-					var img = item.imageurl;
+					var imgage = item.imageurl;
 					document.getElementById('ajaxWord'+(i+1)).innerHTML = text;
-					document.getElementById('ajaxWord'+(i+1)).setAttribute('href', img);
-					document.getElementById('ajaxRequest'+(i+1)).setAttribute('src', img);
+					document.getElementById('ajaxWord'+(i+1)).setAttribute('href', imgage);
+					document.getElementById('ajaxRequest'+(i+1)).setAttribute('src', imgage);
+					$('#input').val('');
 				});
 			});
 	}
